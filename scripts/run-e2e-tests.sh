ad517e935262ec426585effd61dd7124c9d34824
Nathan Thompson <nathan.thompson@readytalk.com>
2018-01-08 15:41:17 -0700

    Remove excess commands; change killall to pkillall

--- scripts/run-e2e-tests.sh
+++ scripts/run-e2e-tests.sh
@@ -2,9 +2,6 @@
 # Move to the Protractor test project folder
 cd $HOME
 
-# Remove previous Allure results
-rm -rf allure-results
-
 # Install the necessary npm packages
 npm install
 # Run the Selenium installation script, located in the local node_modules/ directory.
@@ -14,13 +11,7 @@ node ./node_modules/protractor/bin/webdriver-manager update
 # X11 for Ubuntu is not configured! The following configurations are needed for XVFB.
 # Make a new display :21 with virtual screen 0 with resolution 1024x768 24dpi
 Xvfb :10 -screen 0 1920x1080x24 2>&1 >/dev/null &
-# Export the previously created display
-# export DISPLAY=:10.0
 
-# Right now this is not necessary, because of 'directConnect: true' in the 'e2e.conf.js'
-#echo "Starting webdriver"
-#node ./node_modules/protractor/bin/webdriver-manager start [OR webdriver-manager start] &
-#echo "Finished starting webdriver"
 sleep 20
 
 echo "Running test script"
@@ -30,10 +21,8 @@ export RESULT=$?
 
 echo "Test script is completed"
 # Close the XVFB display
-killall Xvfb
+pkillall Xvfb
 # Remove temporary folders
 rm -rf .config .local .pki .cache .dbus .gconf .mozilla
-# Set the file access permissions (read, write and access) recursively for the result folders
-chmod -Rf 777 allure-results test-results
 
 exit $RESULT
